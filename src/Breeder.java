60af36a58d557a29f262dbee3cc1212108d0c31c
Adeeb Sheikh <asheikh4@u.rochester.edu>
2016-01-16 23:31:18 -0500

    Breeder is coming along. It'll generate a single generation -- now speciation.

--- src/Breeder.java
+++ src/Breeder.java
@@ -1,4 +1,87 @@
+import java.util.*;
 
-public class Breeder {
+public class Breeder{
+	
+	private int num;
+	private int nodes;
+	private int innovTotal;
+	private HashMap<Integer, Gene> geneList;
+	private HashMap<Integer, HashMap<Integer, Gene>> generation;
+	
+	public Breeder(int num){
+		
+		this.num = num;
+		nodes = 20;
+		innovTotal = 0;
+		
+		geneList = new HashMap<Integer, Gene>();
+		generation = new HashMap<Integer, HashMap<Integer, Gene>>();
+		
+		makeFirstGen();
+		
+	}
+	
+	private void makeFirstGen(){
+		
+		//Create all possible starting genes
+		for(int i = 0; i < 4; i++){
+			
+			for(int j = 0; j < 16; j++){
+				
+				//even genes are positive connections
+				Gene gene = new Gene(i, j + 4, 1.0, innovTotal);
+				geneList.put(innovTotal, gene);
+				innovTotal++;
+				//odd genes are negative
+				Gene gene2 = new Gene(i, j + 4, -1.0, innovTotal);
+				geneList.put(innovTotal, gene2);
+				innovTotal++;
+				//let's stick to even/odd
+				
+			}
+			
+		}
+		
+		Random random = new Random();
+		
+		for(int i = 0; i < num; i++){
+			
+			int numGenes = 32 + (int)(16.0*random.nextGaussian());
+			
+			if(numGenes > 64)
+				numGenes = 64;
+			if(numGenes < 1)
+				numGenes = 1;
+			
+			HashMap<Integer, Gene> genome = new HashMap<Integer, Gene>();
+			
+			for(int j = 0; j < numGenes; j++){
+				
+				int next = random.nextInt(128);
+				Gene gene = geneList.get(next);
+				
+				//if the gene is even -- positive -- and the genome contains neither it nor its negative
+				if(next % 2 == 0 && !(genome.containsKey(next) || genome.containsKey(next + 1))){
+
+					gene.setWeight((random.nextDouble() + 0.1)*gene.getWeight());
+					genome.put(next, gene);
+					
+				}
+				else if(next % 2 != 0 && !(genome.containsKey(next) || genome.containsKey(next - 1))){
+
+					gene.setWeight((random.nextDouble() + 0.1)*gene.getWeight());
+					genome.put(next, gene);
+					
+				}
+				else
+					j--;
+				
+			}
+			
+			generation.put(i, genome);
+			
+		}
+		
+	}
 
 }
---
+++ src/Gene.java
@@ -0,0 +1,67 @@
+public class Gene {
+	
+	private int innovNum;
+	private int in;
+	private int out;
+	private double weight;
+	private boolean enabled;
+	
+	public Gene(int in, int out, double weight, int innovNum){
+		
+		this.in = in;
+		this.out = out;
+		this.weight = weight;
+		this.innovNum = innovNum;
+		enabled = true;
+		
+	}
+	
+	public boolean getEnabled(){
+		
+		return enabled;
+		
+	}
+	
+	public int getIn(){
+		
+		return in;
+		
+	}
+	
+	public int getOut(){
+		
+		return out;
+		
+	}
+	
+	public double getWeight(){
+		
+		return weight;
+		
+	}
+	
+	public int getInnov(){
+		
+		return innovNum;
+		
+	}
+	
+	public void disable(){
+		
+		enabled = false;
+		
+	}
+	
+	public void enable(){
+		
+		enabled = true;
+		
+	}
+
+	public void setWeight(double weight){
+		
+		this.weight = weight;
+		
+	}
+	
+}
